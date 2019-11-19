809b4921f7bced5845df5abcba7922128bcc814a
Daniel-03 <57966094+daniel-03@users.noreply.github.com>
2019-11-19 16:21:17 -0500

    Calculadoras

---
+++ Ejercicio1.py
@@ -0,0 +1,709 @@
+# Calcular el promedio de ventas de libros
+# Declaracion de variables
+
+venta1,venta2,venta3,=00,00,00
+promedio=0.0
+
+# input
+venta1=250.00
+venta2=250.00
+venta3=300.00
+
+# processing
+
+promedio=(venta1+venta2+venta3)/3
+
+# output
+print("promedio=",promedio)
+
+# Calcular el promedio de notas del ciclo 2019 I
+# Declaracion de variables
+
+nota1,nota2,nota3,nota4=00,00,00,00
+promedio=0.0
+
+# input
+nota1=14
+nota2=16
+nota3=13
+nota4=12
+
+# processing
+
+promedio=(nota1+nota2+nota3+nota4)/4
+
+# output
+print("promedio=",promedio)
+
+# Calcular el promedio de pasajes comprados por 8 personas
+# Declaracion de variables
+
+compra1,compra2,compra3,compra4,compra5,compra6,compra7,compra8=0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
+promedio=0.0
+
+# input
+compra1=40.00
+compra2=45.00
+compra3=60.00
+compra4=40.00
+compra5=45.00
+compra6=60.00
+compra7=60.00
+compra8=40.00
+
+# processing
+
+promedio=(compra1+compra2+compra3+compra4+compra6+compra7+compra8)/8
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular promedio de recibos de agua de enero a diciembre del año 2018
+# Declaracion de variables
+
+recibo1,recibo2,recibo3,recibo4,recibo5,recibo6,recibo7,recibo8,recibo9,recibo10,recibo11,recibo12=0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
+promedio=0.0
+
+# input
+recibo1=42.30
+recibo2=45.00
+recibo3=39.00
+recibo4=35.60
+recibo5=44.50
+recibo6=35.80
+recibo7=37.00
+recibo8=35.80
+recibo9=45.00
+recibo10=38.00
+recibo11=39.50
+recibo12=41.00
+
+# processing
+
+promedio=(recibo1+recibo2+recibo3+recibo4+recibo5+recibo6+recibo7+recibo8+recibo9+recibo10+recibo11+recibo12)/12
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular el numero promedio de semana de viajes de una combi de illimo a chiclayo si por dia hace 5 a 7 viajes
+# Declaracion de variables
+
+dia1,dia2,dia3,dia4,dia5,dia6,dia7=0,0,0,0,0,0,0
+promedio=0.0
+
+# input
+dia1=6
+dia2=6
+dia3=7
+dia4=5
+dia5=4
+dia6=7
+dia7=7
+
+# processing
+
+promedio=(dia1+dia2+dia3+dia4+dia5+dia6+dia7)/7
+
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular el promedio mensual de 6 trabajadores de la empresa gandules
+# Declaracion de variables
+
+trabajador1,trabajador2,trabajador3,trabajador4,trabajador5,trabajador6=00,00,00,00,00,00
+promedio=0.0
+
+# input
+trabajador1=2550.00
+trabajador2=1800.00
+trabajador3=3200.00
+trabajador4=4550.00
+trabajador5=5500.00
+trabajador6=4100.00
+
+# processing
+
+promedio=(trabajador1+trabajador2+trabajador3+trabajador4+trabajador5+trabajador6)/6
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular el promedio de horas que tardan 5 celulares en cargar
+# Declaracion de variables
+
+hora1,hora2,hora3,hora4,hora5=00,00,00,00,00
+promedio=0.0
+
+# input
+hora1=3.10
+hora2=2.59
+hora3=3.00
+hora4=3.5
+hora5=3.15
+
+# processing
+
+promedio=(hora1+hora2+hora3+hora4+hora5)/5
+
+# output
+
+print("promedio=",promedio)
+
+
+# Calcular el numero de alumnos que asisten 5 a 6 dias a la semana de clase de las UNPRG, sis sabe que el total alumnos es 25
+# Declaracion de variables
+
+dia1,dia2,dia3,dia4,dia5,dia6=00,00,00,00,00,00
+promedio=0.0
+
+# input
+dia1=22
+dia2=25
+dia3=18
+dia4=20
+dia5=25
+dia6=22
+
+# processing
+
+promedio=(dia1+dia2+dia3+dia4+dia5+dia6)/6
+
+
+# output
+print("promedio=",promedio)
+
+# Calcular la suma de los promedios 10 alumnos de estadistica
+# Declaracion de variables
+
+promedio1,promedio2,promedio3,promedio4,promedio5,promedio6,promedio7,promedio8,promedio9,promedio10=00,00,00,00,00,00,00,00,00,00
+promedio=0.0
+
+# input
+promedio1=14
+promedio2=16
+promedio3=13
+promedio4=12
+promedio5=15
+promedio6=15
+promedio7=17
+promedio8=12
+promedio9=14
+promedio10=13
+# processing
+
+suma=(promedio1+promedio2+promedio3+promedio4+promedio5+promedio6+promedio7+promedio8+promedio9+promedio10)
+
+
+# output-
+print("suma=",suma)
+
+# Calcular el promedio de goles de siete jugadores de la selecion peruana en las eliminatorias rusia 2018
+# Declaracion de variables
+
+jugador1,jugador2,jugador3,jugador4,jugador5,jugador6,jugador7=0,0,0,0,0,0,0
+promedio=0.0
+
+# input
+jugador1=8
+jugador2=13
+jugador3=6
+jugador4=5
+jugador5=5
+jugador6=3
+jugador7=2
+# processing
+
+promedio=(jugador1+jugador2+jugador3+jugador4+jugador5+jugador6+jugador7)/7
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular el promedio de puntaje minimo de los 10 primeros puestos a la UNPRG2018-II
+# declaracion de variables
+
+puntaje1,puntaje2,puntaje3,puntaje4,puntaje5,puntaje6,puntaje7,puntaje8,puntaje9,puntaje10=00,00,00,00,00,00,00,00,00,00
+promedio=0.0
+
+# input
+puntaje1=98.9
+puntaje2=98.5
+puntaje3=97.6
+puntaje4=97.2
+puntaje5=96.5
+puntaje6=96.0
+puntaje7=95.9
+puntaje8=95.8
+puntaje9=94.5
+puntaje10=93.9
+
+# processing
+
+promedio=(puntaje1+puntaje2+puntaje3+puntaje4+puntaje5+puntaje6+puntaje7+puntaje8+puntaje9+puntaje10)/10
+
+# output
+print("promedio=",promedio)
+
+# Calcular el numero de emigranres venezolanos en el peru durante los seis primeros meses del año 2019
+# Declaracion de variables
+
+enero1,febreo2,marzo3,abril4,mayo5,junio6=00,00,00,00,00,00
+promedio=0.0
+
+# input
+enero1=10000
+febreo2=12000
+marzo3=13000
+abril4=12000
+mayo5=8000
+junio6=9000
+
+
+# processing
+
+promedio=(enero1+febreo2+marzo3+abril4+mayo5+junio6)/6
+
+# output
+print("promedio=",promedio)
+
+# Calcular el promedio de espectadores de la pelicula del rey leon durante la primera semana de estreno
+# Declaracion de variables
+
+dia1,dia2,dia3,dia4,dia5,dia6,dia7=00,00,00,00,00,00,00
+promedio=0.0
+
+# input
+dia1=50
+dia2=44
+dia3=45
+dia4=42
+dia5=40
+dia6=40
+dia7=40
+
+# processing
+
+promedio=(dia1+dia2+dia3+dia4+dia5+dia6+dia7)/7
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular costo promedio de entradas del concierto de grupo 5 sambiendo que tiene 4 etapas  de ventas
+# Declaracion de variables
+
+etapa1,etapa2,etapa3,etapa4=00,00,00,00
+promedio=0.0
+
+# input
+etapa1=1250
+etapa2=950
+etapa3=800
+etapa4=800
+
+# processing
+
+promedio=(etapa1+etapa2+etapa3+etapa4)/4
+
+# output
+print("promedio=",promedio)
+
+
+# Calcular el numero promedio de paciente que se atienden en la cliica pacifico durante los 4 primeros meses del año 2019
+# Declaracion de variables
+
+mes1,mes2,mes3,mes4=00,00,00,00
+promedio=0.0
+
+# input
+mes1=36
+mes2=42
+mes3=40
+mes4=30
+# processing
+
+promedio=(mes1+mes2+mes3+mes4)/4
+
+
+# output
+print("promedio=",promedio)
+
+# Calcular el promedio en que demoran en correr 7 atletas una pista de 100 metros
+# Declaracion de variables
+
+at1eta1,atleta2,atleta3,atleta4,atleta5,atleta6,atleta7=0.0,0.0,0.0,0.0,0.0,0.0,0.0
+promedio=0.0
+
+# input
+atleta1=0.30
+atleta2=0.35
+atleta3=0.33
+atleta4=0.34
+atleta5=0.31
+atleta6=0.32
+atleta7=0.29
+# processing
+
+promedio=(at1eta1+atleta2+atleta3+atleta4+atleta5+atleta6+atleta7)/7
+
+
+# output
+print("promedio=",promedio)
+
+
+#PROMEDIO DE NOTAS_1
+#DECLARACION
+nota1,nota2,nota3=0.0,0.0,0.0
+promedio=0.0
+codigo=""
+
+#INPUT
+nota1=16
+nota2=18
+nota3=13
+
+#PROCESSING
+promedio=(nota1+nota2+nota3)/3
+
+#OUTPUT
+print("promedio:",promedio)
+
+
+#BOLETA DE PAGO_2
+#DECLARACION
+cantidad1,monto1,monto2,monto3=0,0.0,0.0,0.0
+monto_total=0.0
+
+#INPUT
+cantidad=3
+monto1=90
+monto2=30
+monto3=15.50
+
+#PROCESSING
+cantidad=3
+monto_total=(monto1+monto2+monto3)
+
+#OUTPUT
+print("cantidad:",cantidad)
+print("monto_total:",monto_total)
+
+
+
+#CRONOGRAMA DE PAGOS_3
+#DECLARACION
+pago1,pago2,pago3,pago4=0.0,0.0,0.0,0.0
+pago_total=0.0
+
+#INPUT
+pago1=220
+pago2=220
+pago3=220
+pago4=220
+
+#PROCESSING
+pago_total=(pago1+pago2+pago3+pago4)
+
+#OUTPUT
+print("pago_total:",pago_total)
+
+
+
+#BOLETA ELECTRONICA_4
+#DECLARACION
+importe_total,mporte_a_pagar,igv=0.0,0.0,.0
+total_a_pagar=0.0
+
+#INPUT
+importe_total=3.4
+importe_a_pagar=3.4
+igv=5.72
+
+#PROCESSING
+total_a_pagar=(importe_total+importe_a_pagar+igv)
+
+#OUTPUT
+print("total_a_pagar:",total_a_pagar)
+
+
+
+#CERTIFICADO DE ESTUDIOS_5
+#DECLARACION
+institucion,comportamiento,matematica,arte,educacion="","",0,0,0
+promedio=0.0
+
+#INPUT
+institucion="juan_pablo"
+comportamiento="a"
+matematica=18
+arte=16
+educacion=18
+
+#PROCESSING
+promedio=(matematica+arte+educacion)/3
+
+#OUTPUT
+print("promedo:",promedio)
+
+
+#RECIBO DE AGUA VENCIDO_6
+#DECLARACION
+enero,febrero,igv=0.0,0.0,0.0
+
+#INPUT
+enero=52.30
+febrero=54.82
+igv=45.2
+
+#PROCESSING
+total_a_pagar=(enero+febrero+igv)
+
+#OUTPUT
+print("total_a_pagar:",total_a_pagar)
+
+
+
+#DISTRIBUCION DE PAGOS_7
+#DECLARACION
+nombre,intereses,capital,inteses_compensatorios="",0.0,0.0,0.0,
+
+#INPUT
+nombre="juan"
+intereses=37.01
+capital=113.3
+inteses_compensatorios=1.43
+
+#PROCESSING
+interes_total=(intereses+capital+inteses_compensatorios)
+
+#OUTPUTf
+print("interes_total:",interes_total)
+
+
+# CALCULAR EL NUMERO TOTAL DE DENUNCIAS HECHAS POR ROBOS DURANTE LOS 5 PRIMEROS MESES DEL AÑO
+#DECLARACION DE VARIABLES
+nombre,mes1,mes2,mes3,mes4,mes5="",00,00,00,00,00
+# input
+nombre="comisaria"
+mes1=25
+mes2=30
+mes3=35
+mes4=30
+mes5=25
+
+# processing
+numero_total_de_denuncias=(mes1+mes2+mes3+mes4+mes5)
+
+# output
+print("numero total de dencias:",numero_total_de_denuncias)
+
+# calcular el numero de llamadas telefonicas hechas por carlos a 4 operadoras diferentes
+# declaracion de variables
+nombre,movitar,claro,entel,bitel="",00,00,00,00
+# input
+nombre=" carlos "
+movitar=12
+claro=10
+entel=8
+bitel=10
+
+# processing
+numero_total_de_llamadas=(movitar+claro+entel+bitel)
+
+#output
+print("numero total de llamadas:", numero_total_de_llamadas)
+
+
+# calcular el numero promedio de mensajes enviados por wsp durante 8 horas
+# declaracion de variables
+nombre,hora1,hora2,hora3,hora4,hora5,hora6,hora7,hora8="",00,00,00,00,00,00,00,00
+nombre="wsp"
+hora1=120
+hora2=100
+hora3=130
+hora4=90
+hora5=80
+hora6=85
+hora6=125
+hora7=110
+hora8=105
+
+# processing
+
+promedio_total_de_mensajes=(hora1+hora2+hora3+hora4+hora5+hora6+hora7+hora8)/8
+
+# output
+print("promedio total de mensajes",promedio_total_de_mensajes)
+
+
+# calcular el numero de profesores de la facfym teniendo en cuenta que son 5 escuelas
+# declaracion de variables
+
+nombre,estadistica,matematica,fisica,informatica,electronica="",00,00,00,00,00
+
+# input
+nombre="facfym"
+estadistica=15
+matematica=17
+fisica=14
+informatica=15
+electronica=16
+
+# processing
+
+numero_total_de_profesores=(estadistica+matematica+fisica+informatica+electronica)
+
+# output
+print("numero total de profesores:", numero_total_de_profesores)
+
+
+# calcular el numero de promedio de canciones producidas en 4 horas
+# declaracion de variables
+
+nombre,hora1,hora2,hora3,hora4="",00,00,00,00
+
+# input
+nombre="canciones producidas "
+hora1=20
+hora2=22
+hora3=18
+hora4=16
+
+
+# processing
+
+promedio_total_de_caciones=(hora1+hora2+hora3+hora4)/4
+
+# output
+print("promedio total de caciones:", promedio_total_de_caciones)
+
+# calcular el numero de minutos en que tarda un zapatero en arreglar 5 zaptos
+# declaracion de variables
+
+zapato1,zapato2,zapato3,zapato4,zapato5=00,00,00,00,00
+
+# input
+zapato1=0.15
+zapato2=0.17
+zapato3=0.14
+zapato4=0.16
+zapato5=0.16
+
+# processing
+
+minutos_en_arreglar=(zapato1+zapato2+zapato3+zapato4+zapato5)
+
+# output
+print("minutos en arreglar:", minutos_en_arreglar)
+
+# calcular el costo promedio de las 7 computadoras compradas  teniendo en cuenta que son de marcas distintas
+# declaracion de variables
+
+computadora1,computadora2,computadora3,computadora4,computadora5,computadora6,computadora7=00,00,00,00,00,00,00
+
+# input
+computadora1=2100
+computadora2=2250
+computadora3=2150
+computadora4=2000
+computadora5=2800
+computadora6=2500
+computadora7=2300
+
+
+# processing
+
+costo_promedio_de_computadoras=(computadora1+computadora2+computadora3+computadora4+computadora5+computadora6+computadora7)/7
+
+# output
+print("costo promedio de computadoras:", costo_promedio_de_computadoras)
+
+
+# calcular las ventas de celulares samsung A30 de los 6 primeros meses de año chiclayo
+# declaracion de variables
+
+mes1,mes2,mes3,mes4,mes5,mes6=00,00,00,00,00,00
+
+# input
+mes1=15000
+mes2=24500
+mes3=22564
+mes3=18000
+mes4=22000
+mes5=20000
+mes6=18000
+
+# processing
+
+venta_total=(mes1+mes2+mes3+mes4+mes5+mes6)
+
+# output
+print("venta total:", venta_total)
+
+
+
+# calcular numero total de autos fabricados en los años 2016,2017,2018
+# declaracion de variables
+
+año1,año2,año2=00,00,00
+
+# input
+año1=2600
+año2=2800
+año3=3200
+
+
+# processing
+
+numero_total_de_autos_fabricados=(año1+año2+año3)
+
+# output
+print("numero total de autos fabricados:", numero_total_de_autos_fabricados)
+
+
+# calcular el total de solititudes entregadas para cambios de grupo en la ecuela de estadistica
+# teniendo en cuenta los codigos 2016I hasta el 2017II
+# declaracion de variables
+
+nombre,cd2016I,cd2016II,cd2017I,cd2017II="",00,00,00,00
+
+# input
+nombre="estadistica"
+cd2016I=15
+cd2016II=12
+cd2017I=10
+cd2017II=8
+
+# processing
+
+calcular_el_tota_de_solitudes=(cd2016I+cd2016II+cd2017I+cd2017II)
+
+# output
+print("calcular el total de solicitudes:", calcular_el_tota_de_solitudes)
+
+
+# calcular el numero de de alumnos que entran el bus con una capacidad de 40 asientos
+# teniendo en cuenta que solo hay 5 buses disponibles
+# declaracion de variables
+
+bus1,bus2,bus3,bus4,bus5=00,00,00,00,00
+
+# input
+
+bus1=40
+bus2=45
+bus3=35
+bus4=42
+bus5=39
+
+# processing
+
+numero_de_alumnos_en_el_bus=(bus1+bus2+bus3+bus4+bus5)
+
+# output
+print("numero de alumnos en el bus:", numero_de_alumnos_en_el_bus)
